d69bc25a1d66fd5a0af9f5b3b5873751ed5dc90a
FrankCW <wongasa999@gmail.com>
2024-09-16 11:13:38 +0700

    CallBack

--- ex-2.js
+++ ex-2.js
@@ -1,15 +1,22 @@
 //Exercise #2: At Least Five Function
 
 function atLeastFive(array, operation) {
-  // Start coding here
+  const count = array.filter(operation).length;
+  return count >= 5;
 }
 
-const studentScoresRoom1 = [90, 40, 67, 80, 100, 15, 86, 12, 99, 67];
+const studentScoresRoom1 = [12, 40, 67, 80, 100, 15, 40, 78, 75, 80];
 const studentScoresRoom2 = [78, 98, 23, 15, 40, 12, 40, 67, 80, 100];
 const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
 
-// Using `atLeastFive` function here
+function isGreaterThan70(score) {
+  return score > 70;
+}
 
-let scoreRoom1Result;
-let scoreRoom2Result;
-let scoreRoom3Result;
+let scoreRoom1Result = atLeastFive(studentScoresRoom1, isGreaterThan70);
+let scoreRoom2Result = atLeastFive(studentScoresRoom2, isGreaterThan70);
+let scoreRoom3Result = atLeastFive(studentScoresRoom3, isGreaterThan70)
+
+console.log(`นักเรียนห้องที่ 1 ${scoreRoom1Result ? 'ผ่านเกณฑ์ ' : 'ไม่ผ่านเกณฑ์ '}`);
+console.log(`นักเรียนห้องที่ 2 ${scoreRoom2Result ? 'ผ่านเกณฑ์ ' : 'ไม่ผ่านเกณฑ์ '}`);
+console.log(`นักเรียนห้องที่ 3 ${scoreRoom3Result ? 'ผ่านเกณฑ์ ' : 'ไม่ผ่านเกณฑ์ '}`);
